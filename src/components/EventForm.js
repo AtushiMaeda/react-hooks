1cc078d064efa6ea7b5c33b9057d608e5cdc0158
AtsushiMaeda <tachidori@gmail.com>
2020-10-25 12:50:21 +0900

    create operation logs

--- src/components/EventForm.js
+++ src/components/EventForm.js
@@ -1,6 +1,12 @@
 import React, { useContext, useState } from 'react';
-import { CREATE_EVENT, DELETE_ALL_EVENTS } from '../actions';
-import AppContext from '../contexts/AppContext'
+import {
+  CREATE_EVENT,
+  DELETE_ALL_EVENTS,
+  ADD_OPPERATION_LOG,
+  DELETE_ALL_OPPERATION_LOGS
+} from '../actions';
+import AppContext from '../contexts/AppContext';
+import { timeCurrentIso8601 } from '../utils'
 
 const EventForm = () => {
   const { state, dispatch } = useContext(AppContext);
@@ -15,6 +21,12 @@ const EventForm = () => {
       body,
     });
 
+    dispatch({
+      type: ADD_OPPERATION_LOG,
+      description: 'イベントを作成しました。',
+      operatedAt: timeCurrentIso8601()
+    });
+
     setTitle('');
     setBody('');
   };
@@ -24,6 +36,11 @@ const EventForm = () => {
     const result = window.confirm('全てのイベントを本当に削除しても良いですか？');
     if (result) {
       dispatch({ type: DELETE_ALL_EVENTS });
+      dispatch({
+        type: ADD_OPPERATION_LOG,
+        description: '全てのイベントを削除しました。',
+        operatedAt: timeCurrentIso8601();
+      })
     }
   };
 
